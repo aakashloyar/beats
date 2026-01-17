884a7d6944fa6981157376763e32b51006c69e98
Aakash Loyar <aakashloyar@gmail.com>
2026-01-17 21:48:33 +0530

    language type

---
+++ track/internal/domain/language.go
@@ -0,0 +1,10 @@
+package domain
+
+type Language string
+
+const (
+	LanguageHindi    Language = "hi"
+	LanguageEnglish  Language = "en"
+	LanguageHaryanvi Language = "hr"
+	LanguagePunjabi  Language = "pn"
+)
