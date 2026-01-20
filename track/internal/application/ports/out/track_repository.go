816fcb3ae873910904dbccbb769512b7d3db6b75
Aakash Loyar <aakashloyar@gmail.com>
2026-01-20 12:24:50 +0530

    fix(track): fix spelling in TrackRepo interface

--- track/internal/application/ports/out/track_repository.go
+++ track/internal/application/ports/out/track_repository.go
@@ -1,6 +1,7 @@
 package out
 import "github.com/aakashloyar/beats/track/internal/domain"
-type TrackRespository interface {
+
+type TrackRepository interface {
 	Save(track *domain.Track) error
 	FindById(id string) (*domain.Track, error)
 	FindByArtist(artistID string) (*domain.Track, error)
