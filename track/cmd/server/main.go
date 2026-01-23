7ea26bc7479e6e8ee8c3553f9d539de229945610
Aakash Loyar <aakashloyar@gmail.com>
2026-01-23 13:04:14 +0530

    initialise postgres and track service in main

--- track/cmd/server/main.go
+++ track/cmd/server/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	//"database/sql"
+	"log"
+	"net/http"
+	//httpartist "github.com/aakashloyar/beats/track/internal/adapters/in/http/artist"
+	httptrack "github.com/aakashloyar/beats/track/internal/adapters/in/http/track"
+	postgres   "github.com/aakashloyar/beats/track/internal/adapters/out/postgres"
+	tracksvc   "github.com/aakashloyar/beats/track/internal/application/service/track"
+	//artistsvc  "github.com/aakashloyar/beats/track/internal/application/service/artist"
+	"github.com/aakashloyar/beats/track/internal/application/ports/out/system"
+
+)
+
+
 func main() {
-	
+
+	config := postgres.Config{
+		Host     : "",
+		Port     : 123, 
+		User     : "",
+		Password : "",
+		DBName   : "",
+		SSLMode  : "", 
+	}
+
+	clock := system.SystemClock{}
+    idGen := system.UUIDGenerator{}
+
+	db, err := config.NewDB()
+
+	if err !=nil {
+        log.Fatalf("failed to open DB: %v", err)
+	}
+
+	trackRepo := postgres.NewTrackRepository(db)
+	//audio_variantsRepo := postgres.NewAudioVariantRepository(db)
+
+	createtrackService := tracksvc.NewCreateTrackService(trackRepo,clock,idGen)
+
+	gettrackService := tracksvc.NewGetTrackService(trackRepo)
+
+	trackHandler := httptrack.NewHandler(createtrackService,gettrackService)
+
+
+
+	mux := http.NewServeMux()
+	httptrack.RegisterRoutes(mux,trackHandler)
+	//artist.ResisterRoutes(mux,artistHandler)
+
+	http.ListenAndServe(":8080", mux)
 }
