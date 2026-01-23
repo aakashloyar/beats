9e556481327572e8f9142e4042bc25ae70511d17
Aakash Loyar <aakashloyar@gmail.com>
2026-01-23 13:02:13 +0530

    refactor: update FindByArtist to return multiple tracks

--- track/internal/application/ports/out/track_repository.go
+++ track/internal/application/ports/out/track_repository.go
@@ -4,6 +4,6 @@ import "github.com/aakashloyar/beats/track/internal/domain"
 type TrackRepository interface {
 	Save(track *domain.Track) error
 	FindById(id string) (*domain.Track, error)
-	FindByArtist(artistID string) (*domain.Track, error)
+	FindByArtist(artistID string) ([]*domain.Track, error)
 }
 
