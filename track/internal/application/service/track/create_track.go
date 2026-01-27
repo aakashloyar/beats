825ea033d5e249d23679372ed299ea936f4a031e
Aakash Loyar <aakashloyar@gmail.com>
2026-01-27 22:42:58 +0530

    chore: gofmt

--- track/internal/application/service/track/create_track.go
+++ track/internal/application/service/track/create_track.go
@@ -1,45 +1,45 @@
-package track
+package service
 
 import (
+	"context"
+	"errors"
+	"github.com/aakashloyar/beats/track/internal/application/ports/in/track"
 	"github.com/aakashloyar/beats/track/internal/application/ports/out"
-    "github.com/aakashloyar/beats/track/internal/application/ports/in/track"
-    "github.com/aakashloyar/beats/track/internal/domain"
-    "errors"
-    "context"
+	"github.com/aakashloyar/beats/track/internal/domain"
 )
 
 type CreateTrackService struct {
-    trackRepo out.TrackRepository
-    clock     domain.Clock
-    idGen     domain.IDGenerator
+	trackRepo out.TrackRepository
+	idGen     domain.IDGenerator
+	clock     domain.Clock
 }
 
-func NewCreateTrackService( trackRepo out.TrackRepository, clock domain.Clock, idGen domain.IDGenerator) in.CreateTrackService {
-    return &CreateTrackService{
-        trackRepo: trackRepo,
-        clock : clock,
-        idGen : idGen,
-    }
+func NewCreateTrackService(trackRepo out.TrackRepository, idGen domain.IDGenerator, clock domain.Clock) in.CreateTrackService {
+	return &CreateTrackService{
+		trackRepo: trackRepo,
+		clock:     clock,
+		idGen:     idGen,
+	}
 }
 
-func (s *CreateTrackService) Execute(ctx context.Context, input in.CreateTrackInput) (in.CreateTrackOutput,error){
-    if input.Title == "" {
-        return in.CreateTrackOutput{}, errors.New("Title is required")
-    }
-    track:=domain.Track{
-        ID: s.idGen.NewID(),
-        Title: input.Title,
-        ArtistID: input.ArtistID,
-        AlbumID: input.AlbumID,
-        CoverImageURL: input.CoverImageURL,
-        DurationMS: input.DurationMS,
-        Language: input.Language,
-        ReleaseDate: input.ReleaseDate,
-        CreatedAt: s.clock.Now(),
-    }
-    if err := s.trackRepo.Save(track) ; err != nil {
-        return in.CreateTrackOutput{}, err
-    }
-    return in.CreateTrackOutput{TrackID: track.ID}, nil
+func (s *CreateTrackService) Execute(ctx context.Context, input in.CreateTrackInput) (in.CreateTrackOutput, error) {
+	if input.Title == "" {
+		return in.CreateTrackOutput{}, errors.New("Title is required")
+	}
+	track := domain.Track{
+		ID:            s.idGen.NewID(),
+		Title:         input.Title,
+		ArtistID:      input.ArtistID,
+		AlbumID:       input.AlbumID,
+		CoverImageURL: input.CoverImageURL,
+		DurationMS:    input.DurationMS,
+		Language:      input.Language,
+		ReleaseDate:   input.ReleaseDate,
+		CreatedAt:     s.clock.Now(),
+	}
+	if err := s.trackRepo.Save(track); err != nil {
+		return in.CreateTrackOutput{}, err
+	}
+	return in.CreateTrackOutput{TrackID: track.ID}, nil
 
 }
