c4ddab8670d342f7a89e6d87c8af8e0b8e6d600a
Aakash Loyar <aakashloyar@gmail.com>
2026-01-27 21:20:13 +0530

    fix(track): listracksrepo input change to domain

--- track/internal/application/service/track/list_tracks.go
+++ track/internal/application/service/track/list_tracks.go
@@ -1,10 +1,10 @@
-package track 
+package service
 
 import (
-	"github.com/aakashloyar/beats/track/internal/application/ports/out"
-	"github.com/aakashloyar/beats/track/internal/application/ports/in/track"
-	"github.com/aakashloyar/beats/track/internal/domain"
 	"context"
+	"github.com/aakashloyar/beats/track/internal/application/ports/in/track"
+	"github.com/aakashloyar/beats/track/internal/application/ports/out"
+	"github.com/aakashloyar/beats/track/internal/domain"
 )
 
 type ListTracksService struct {
@@ -17,11 +17,31 @@ func NewListTracksService(trackRepo out.TrackRepository) in.ListTracksService {
 	}
 }
 
-func (s *ListTracksService) Execute(ctx context.Context,input in.ListTracksInput) ([]domain.Track,error) {
-	x,err := s.trackRepo.ListTracks(input)
+func (s *ListTracksService) Execute(ctx context.Context, input in.ListTracksInput) ([]in.ListTrackOutput, error) {
+	x, err := s.trackRepo.ListTracks(domain.TrackFilter{
+		Title:    &input.Title,
+		ArtistID: &input.ArtistID,
+		AlbumID:  &input.AlbumID,
+		Limit:    &input.Limit,
+		Offset:   &input.Offset,
+	})
 	if err != nil {
-		return nil ,err 
+		return nil, err
 	}
-	return x, nil 
-}
+	var tracks []in.ListTrackOutput
+	for _, each := range x {
+		track := in.ListTrackOutput{
+			ID:            each.ID,
+			ArtistID:      each.ArtistID,
+			AlbumID:       each.AlbumID,
+			CoverImageURL: each.CoverImageURL,
+			DurationMS:    each.DurationMS,
+			Language:      each.Language,
+			ReleaseDate:   each.ReleaseDate,
+			CreatedAt:     each.CreatedAt,
+		}
+		tracks = append(tracks, track)
+	}
 
+	return tracks, nil
+}
