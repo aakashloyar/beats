2d858923a3b06ea2bbec962702559369cc723e34
Aakash Loyar <aakashloyar@gmail.com>
2026-01-27 10:55:14 +0530

    feat(track): add ListTracksService

---
+++ track/internal/application/service/track/list_tracks.go
@@ -0,0 +1,27 @@
+package track 
+
+import (
+	"github.com/aakashloyar/beats/track/internal/application/ports/out"
+	"github.com/aakashloyar/beats/track/internal/application/ports/in"
+	"github.com/aakashloyar/beats/track/internal/domain"
+	"context"
+)
+
+type ListTracksService struct {
+	trackRepo out.TrackRepository
+}
+
+func NewListTracksService(trackRepo out.TrackRepository) in.ListTracksService {
+	return &ListTracksService{
+		trackRepo: trackRepo,
+	}
+}
+
+func (s *ListTracksService) Execute(ctx context.Context,input in.ListTracksInput) ([]domain.Track,error) {
+	x,err := s.trackRepo.ListTracks(input)
+	if err != nil {
+		return nil ,err 
+	}
+	return x, nil 
+}
+
