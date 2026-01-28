6e2c6131e6d260f26f700f90ecbb8e12678c8999
Aakash Loyar <aakashloyar@gmail.com>
2026-01-28 07:36:40 +0530

    refractor(track): removing depencedence from in apapter

--- track/internal/application/ports/out/track_repository.go
+++ track/internal/application/ports/out/track_repository.go
@@ -1,12 +1,11 @@
 package out
+
 import (
 	"github.com/aakashloyar/beats/track/internal/domain"
-	"github.com/aakashloyar/beats/track/internal/application/ports/in/track"
 )
 
 type TrackRepository interface {
 	Save(track domain.Track) error
-	FindByID(id string) (domain.Track, error)
-	ListTracks(input in.ListTracksInput) ([]domain.Track, error)
+	FindByID(trackID string) (domain.Track, error)
+	ListTracks(input domain.TrackFilter) ([]domain.Track, error)
 }
-
