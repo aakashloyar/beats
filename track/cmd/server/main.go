b661804c0eedf74cee7ce8b198e3f4d2b9a3cca3
Aakash Loyar <aakashloyar@gmail.com>
2026-01-28 07:53:35 +0530

    chore(artist): register artist route service handler

--- track/cmd/server/main.go
+++ track/cmd/server/main.go
@@ -2,54 +2,53 @@ package main
 
 import (
 	//"database/sql"
+	httpartist "github.com/aakashloyar/beats/track/internal/adapters/in/http/artist"
+	httptrack "github.com/aakashloyar/beats/track/internal/adapters/in/http/track"
+	postgres "github.com/aakashloyar/beats/track/internal/adapters/out/postgres"
+	"github.com/aakashloyar/beats/track/internal/application/ports/out/system"
+	artistsvc "github.com/aakashloyar/beats/track/internal/application/service/artist"
+	tracksvc "github.com/aakashloyar/beats/track/internal/application/service/track"
 	"log"
 	"net/http"
-	//httpartist "github.com/aakashloyar/beats/track/internal/adapters/in/http/artist"
-	httptrack "github.com/aakashloyar/beats/track/internal/adapters/in/http/track"
-	postgres   "github.com/aakashloyar/beats/track/internal/adapters/out/postgres"
-	tracksvc   "github.com/aakashloyar/beats/track/internal/application/service/track"
-	//artistsvc  "github.com/aakashloyar/beats/track/internal/application/service/artist"
-	"github.com/aakashloyar/beats/track/internal/application/ports/out/system"
-
 )
-
 
 func main() {
 
 	config := postgres.Config{
-		Host     : "",
-		Port     : 123, 
-		User     : "",
-		Password : "",
-		DBName   : "",
-		SSLMode  : "", 
+		Host:     "",
+		Port:     123,
+		User:     "",
+		Password: "",
+		DBName:   "",
+		SSLMode:  "",
 	}
 
 	clock := system.SystemClock{}
-    idGen := system.UUIDGenerator{}
+	idGen := system.UUIDGenerator{}
 
 	db, err := config.NewDB()
 
-	if err !=nil {
-        log.Fatalf("failed to open DB: %v", err)
+	if err != nil {
+		log.Fatalf("failed to open DB: %v", err)
 	}
 
 	trackRepo := postgres.NewTrackRepository(db)
-	//audio_variantsRepo := postgres.NewAudioVariantRepository(db)
+	artistRepo := postgres.NewArtistRepository(db)
 
-	createtrackService := tracksvc.NewCreateTrackService(trackRepo,clock,idGen)
-
+	createtrackService := tracksvc.NewCreateTrackService(trackRepo, idGen, clock)
 	gettrackService := tracksvc.NewGetTrackService(trackRepo)
-
-
 	listtracksService := tracksvc.NewListTracksService(trackRepo)
 
-	trackHandler := httptrack.NewHandler(createtrackService,gettrackService, listtracksService)
+	trackHandler := httptrack.NewHandler(createtrackService, gettrackService, listtracksService)
 
+	createartistService := artistsvc.NewCreateTrackService(artistRepo, idGen, clock)
+	getartistService := artistsvc.NewGetArtistService(artistRepo)
 
+	artistHandler := httpartist.NewHandler(createartistService, getartistService)
 
 	mux := http.NewServeMux()
-	httptrack.RegisterRoutes(mux,trackHandler)
+	httptrack.RegisterRoutes(mux, trackHandler)
+	httpartist.RegisterRoutes(mux, artistHandler)
 	//artist.ResisterRoutes(mux,artistHandler)
 
 	http.ListenAndServe(":8080", mux)
